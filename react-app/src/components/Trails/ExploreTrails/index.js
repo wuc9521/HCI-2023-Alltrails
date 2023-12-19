4136d21ed27a2ca110bd063495e145fa4b593340
wuc9521 <wuc9521@gmail.com>
2023-12-20 01:55:05 +0800

    async loading data to explore page

--- react-app/src/components/Trails/ExploreTrails/index.js
+++ react-app/src/components/Trails/ExploreTrails/index.js
@@ -1,51 +1,36 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getSingleListThunk, editListThunk } from "../../../store/lists";
+import { getTrailsThunk } from "../../../store/trails";
 import { useParams, useHistory } from "react-router-dom";
 import TrailItem from "../../Trails/TrailItem";
 import Map from "../../Map";
 import "./ExploreTrails.css";
 
 function ExploreTrails() {
-  const { listId } = useParams();
   const dispatch = useDispatch();
   const history = useHistory();
-
-  const list = useSelector((state) => state.lists);
-
-  const handleClick = (e) => {
-    history.push("/")
-  }
-
   useEffect(() => {
-    dispatch(getSingleListThunk(listId));
-  }, [dispatch, listId]);
-
-  if (!list.id) return null;
-  const bookmarks = list.bookmarks;
-
+    dispatch(getTrailsThunk());
+  }, [dispatch]);
+  const allTrails = useSelector((state) => Object.values(state.trails));
   return (
-    <div className="list-details-container">
-      <div className="list-details-content">
+    <div className="explore-details-container">
+      <div className="explore-details-content">
         <div className="explore-details-content-left">
           <div className="list-details-bookmarks">
-            {bookmarks.length ? bookmarks.map(
-              (bookmark, i) => (
-              <TrailItem key={i} trail={bookmark.trail} nameOfClass="bookmark" bookmarkId={bookmark.id} listId={listId} />
-              )
-            )
-              : <div className="no-bookmarks">
-                <p className="secondary-color" style={{ fontWeight: "500" }}>Tap the bookmark icon on any trail to turn this into your adventure wishlist</p>
-                <button
-                  className="green-button"
-                  id="explorer-trails-button"
-                  onClick={(e) => handleClick(e)}
-                >Explorer Trails</button>
-              </div>}
+            <div className="list-details-bookmarks-header">
+              <h2><center>Explore Trails</center></h2>
+            </div>
+            <div className="list-details-bookmarks-content">
+              {allTrails.map((trail) => (
+                <TrailItem key={trail.id} trail={trail} />
+              ))}
+            </div>
           </div>
         </div>
         <div className="list-details-content-right">
-          <Map bookmarks={bookmarks} />
+        {/* <Map bookmarks={allTrails} /> */}
+        {/* 要是js也有多态就好了hhh */}
         </div>
       </div>
     </div>
