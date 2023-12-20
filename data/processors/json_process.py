8af7ac9ad3adad8047deafda16734968b3b4db0b
okoh <2513360276@qq.com>
2023-12-20 21:43:37 +0800

    湖南 is added

--- data/processors/json_process.py
+++ data/processors/json_process.py
@@ -1,16 +1,16 @@
 import json
 
 # 读取 JavaScript 文件
-with open('../raw/Hebei.json', 'r', encoding='utf-8') as file:
+with open('../raw/Hunan.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-exclude = [1,2,3,4,5,6,9,10,11]
+exclude = []
 
 num = 0
 for obj in data:
 
     print(obj['name'])
-    file_name = '../path/hebei/' + obj['name'] + '.js'
+    file_name = '../path/hunan/' + obj['name'] + '.js'
     with open(file_name, 'r') as tfile:
         js_code = tfile.read()
         js_data = json.loads(js_code)
@@ -34,8 +34,8 @@ for obj in data:
             print("-------------------------------")
         num += 1
 
-with open('../reformat/hebei.json', 'w', encoding='utf-8') as file:
-    json.dump(data, file, ensure_ascii=False)
+# with open('../reformat/hunan.json', 'w', encoding='utf-8') as file:
+#     json.dump(data, file, ensure_ascii=False)
 
 
 
