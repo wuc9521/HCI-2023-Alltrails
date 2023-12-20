6216b15058f7a4615c45b7501c0ffb6ee7982edd
okoh <2513360276@qq.com>
2023-12-20 14:17:28 +0800

    中国部分的数据已经全部处理完成

--- gross.py
+++ gross.py
@@ -1,8 +1,9 @@
 import json
 
-with open('', 'r', encoding='utf-8') as file:
+with open('./dataReformat/.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 num = 0
 for obj in data:
     num += 1
+print(num)
